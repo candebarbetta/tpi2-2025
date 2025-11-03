5f63bd9781a15e31062153ab6e24ed6bb7739993
candebarbetta <cbarbetta@udesa.edu.ar>
2025-11-03 17:06:21 -0300

    firebase

---
+++ tpIntegrador2/src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCBaxG-wc9W6XaPyN8Y-WAdyGAz0FJoLkM",
+  authDomain: "tpi2-2025firebase.firebaseapp.com",
+  projectId: "tpi2-2025firebase",
+  storageBucket: "tpi2-2025firebase.firebasestorage.app",
+  messagingSenderId: "956253459085",
+  appId: "1:956253459085:web:92d18b0a9c911642a688c4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
